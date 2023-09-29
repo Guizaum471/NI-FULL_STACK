5ab6b0dca930785b843ea52ac9dc3040bf4fc2dc
Guizaum471 <132626888+Guizaum471@users.noreply.github.com>
2023-09-29 14:58:10 -0300

    Hospedagem

---
+++ script.js
@@ -0,0 +1,9 @@
+window.sr=ScrollReveal({reset:true});
+
+sr.reveal('.area-1', {duration:1000,rotate:{x:0, y:80, z:0}})
+
+sr.reveal('.area-2', {duration:1000,rotate:{x:0, y:80, z:0}})
+
+sr.reveal('.area-3', {duration:1000,rotate:{x:0, y:80, z:0}})
+
+sr.reveal('.area-4', {duration:1000,rotate:{x:0, y:80, z:0}})
